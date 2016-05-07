c17af368dfd39299510d149759b9150a68465562
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2016-05-07 22:33:48 +0300

    Updates the dependencies

--- assets/js/app/templates.js
+++ assets/js/app/templates.js
@@ -3,7 +3,7 @@ window["_templates_app_"] = window["_templates_app_"] || {};
 
 window["_templates_app_"]["about.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '';
 with (obj) {
 __p += '<h1>About me</h1>\r\n<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit ut tempora deserunt obcaecati, sapiente corporis facere exercitationem repellat accusantium atque aperiam, possimus ullam quaerat maxime consequatur sunt iure explicabo veniam!</p>\r\n';
 
@@ -15,7 +15,7 @@ window["_templates_app_"] = window["_templates_app_"] || {};
 
 window["_templates_app_"]["footer.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '';
 with (obj) {
 __p += '<footer>&copy; My App</footer>\r\n';
 
@@ -27,7 +27,7 @@ window["_templates_app_"] = window["_templates_app_"] || {};
 
 window["_templates_app_"]["header.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '';
 with (obj) {
 __p += '<header>\r\n    <div class="logo">\r\n        <a href="#">My App</a>\r\n    </div>\r\n\r\n    <nav class="navigation">\r\n        <ul>\r\n            <li class="navigation__item">\r\n                <a href="#">Home</a>\r\n            </li>\r\n\r\n            <li class="navigation__item">\r\n                <a href="#/about">About</a>\r\n            </li>\r\n        </ul>\r\n    </nav>\r\n</header>\r\n';
 
@@ -39,7 +39,7 @@ window["_templates_app_"] = window["_templates_app_"] || {};
 
 window["_templates_app_"]["homepage.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '';
 with (obj) {
 __p += '<p>Welcome to my app.</p>\r\n\r\n<p>To run the tests go to the <a href="/test/SpecRunner.html">test/SpecRunner.html</a>\r\npage\r\n</p>\r\n';
 
